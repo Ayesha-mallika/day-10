c39a651783ef057b9452aead70e2ca297673e3f7
new repo <ayeshajasmeen79@gmail.com>
2023-11-10 11:20:41 +0530

    initial commit

---
+++ FirstWebSolution/FirstWebApplication/Contexts/ShoppingContext.cs
@@ -0,0 +1,18 @@
+﻿using FirstWebApplication.Constrollers;
+using FirstWebApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstWebApplication.Contexts
+{
+    public class ShoppingContext : DbContext
+    {
+        public ShoppingContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
---
+++ FirstWebSolution/FirstWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+
+namespace FirstWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Landing()
+        {
+            return View();
+        }
+        [ActionName ("Foxy")]
+        public IActionResult CannotBeTheNameInTheResultCozItsReallyLong()
+        {
+            return View();
+        }
+    }
+}
